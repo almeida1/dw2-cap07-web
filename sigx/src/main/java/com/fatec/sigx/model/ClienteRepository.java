a8c4b883b09ed88aa88cb0a62702952b4d901c6c
almei <almei@LAPTOP-102TOTT6>
2022-07-22 08:04:46 -0300

    Inclucao da camada de servico - mantem cliente

---
+++ sigx/src/main/java/com/fatec/sigx/model/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.fatec.sigx.model;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ClienteRepository extends JpaRepository <Cliente, Long>{
+	Optional<Cliente> findByCpf(String cpf);
+	List<Cliente> findAllByNomeIgnoreCaseContaining(String nome);
+}
+

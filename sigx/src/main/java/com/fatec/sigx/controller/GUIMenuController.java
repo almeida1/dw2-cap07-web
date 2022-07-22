090ec87f53dd868f3148404b3a16b26e574edb0d
almei <almei@LAPTOP-102TOTT6>
2022-07-22 08:53:45 -0300

    Inclusão da camada - view

---
+++ sigx/src/main/java/com/fatec/sigx/controller/GUIMenuController.java
@@ -0,0 +1,16 @@
+package com.fatec.sigx.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class GUIMenuController {
+	@GetMapping("/login")
+	public ModelAndView autenticacao() {
+		return new ModelAndView("paginaLogin");
+	}
+
+	@GetMapping("/")
+	public ModelAndView home() {
+		return new ModelAndView ("paginaMenu");
+	}
+}
